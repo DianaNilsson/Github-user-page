54bc71daa3be868421a1e19e9e5e53d20ebdbfc6
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-08-04 11:18:40 +0200

    add menu bar

--- src/components/MainHeaderLeftSidebar.vue
+++ src/components/MainHeaderLeftSidebar.vue
@@ -1,5 +1,12 @@
 <template>
-  <nav id="main-header-left-sidebar"></nav>
+  <nav
+    id="main-header-left-sidebar"
+    :class="{'sidebar-expanded': !sidebarCollapsed, 'sidebar-collapsed': sidebarCollapsed}"
+  >
+    <button class="menu-bar">
+      <img src="../assets/icons/bar.png" alt="menu-bar" @click="menuClick" />
+    </button>
+  </nav>
 </template>
 
 <script>
@@ -18,13 +25,35 @@ export default {
   background-color: rgba(62, 72, 55, 0.3);
   border-right: 2px solid #ffffff;
 
-  /* display: flex;
+  display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding-top: 3.5rem; */
+  padding-top: 3.5rem;
 
-  /* z-index: 1; */
+  z-index: 1;
   transition: width 0.3s;
-  /* white-space: nowrap; */
+  white-space: nowrap;
+}
+
+/* Menu bar */
+.menu-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 3.5rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 2px solid #ffffff;
+}
+
+.menu-bar img {
+  height: 2rem;
+  transition: height 0.1s;
+}
+
+.menu-bar img:hover {
+  height: 2.2rem;
 }
 </style>
