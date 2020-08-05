3feea69674b44a9ce65d1d9025a2ed92cd58f18b
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-08-05 11:15:19 +0200

    hide top sidebar for mobiles

--- src/components/MainHeaderTopSidebar.vue
+++ src/components/MainHeaderTopSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header id="top-sidebar" :class="{'position-left': !sidebarCollapsed}">
+  <header id="main-header-top-sidebar" :class="{'sidebar-expanded': !sidebarCollapsed}">
     <p>Diana Nilsson | Frontendutvecklare</p>
   </header>
 </template>
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style scoped>
-#top-sidebar {
+#main-header-top-sidebar {
   position: absolute;
   top: 0;
   left: 3.6rem;
@@ -32,20 +32,28 @@ export default {
   white-space: nowrap;
 }
 
-#top-sidebar:hover {
+#main-header-top-sidebar:hover {
   background-color: rgba(62, 72, 55, 0.4);
 }
 
-#top-sidebar > p {
+#main-header-top-sidebar > p {
   color: #ffffff;
   font-size: 1.2rem;
   font-weight: 300;
-  letter-spacing: 2px;
+  letter-spacing: 0.125rem;
   text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
 }
 
 /* If sidebar expanded */
-.position-left {
+.sidebar-expanded {
   left: 11rem !important;
+}
+
+/* Responsiveness */
+@media only screen and (max-width: 450px) and (orientation: portrait),
+  only screen and (max-width: 850px) and (max-height: 450px) {
+  #main-header-top-sidebar {
+    display: none;
+  }
 }
 </style>
