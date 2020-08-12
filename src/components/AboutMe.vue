c50deba2d989125247058d0b5d0821040b9b75fa
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-08-12 07:22:46 +0200

    add about me fact boxes

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -7,28 +7,36 @@
           <h2>Om mig</h2>
           <h3>Frontendutvecklare med designtänk</h3>
         </hgroup>
-        <p>Hej där! Jag är en frontendutvecklare som älskar att skapa nya projekt. Jag gillar att planera, detaljarbete och problemlösningen som jag behöver klara av längs vägen. Med programmeringen ser jag tusen möjligheter och jag kan knapptvänta på att få fortsätta utvecklas och få testa nya projekt. Jag är en frontendutvecklare som älskar att skapa nya projekt.</p>
+        <p>Hej där! Jag är en frontendutvecklare som gillar att planera och skapa nya projekt. Jag gillar att planering, detaljarbete och problemlösningen som jag behöver klara av längs vägen. Med programmeringen ser jag tusen möjligheter och jag kan knappt vänta på att få fortsätta utvecklas genom att utöva nya projekt.</p>
         <p class="signature">/Diana Nilsson</p>
       </article>
     </section>
+
+    <about-me-fact />
   </section>
 </template>
 
 <script>
+import AboutMeFact from "./AboutMeFact.vue";
+
 export default {
-  name: "AboutMe"
+  name: "AboutMe",
+  components: {
+    AboutMeFact
+  }
 };
 </script>
 
 <style scoped>
 .about-me {
-  padding: 8rem;
+  padding: 9rem;
   margin-bottom: 10rem;
 }
 .about-me-section {
   display: flex;
-  border-right: 1px solid #588c7e;
-  padding-right: 2rem;
+  /* border-right: 1px solid #588c7e;
+  padding-right: 3rem; */
+  margin-bottom: 8rem;
 }
 
 .about-me-portrait {
@@ -64,61 +72,4 @@ export default {
   color: #b5b2b2;
   margin: 2rem 6rem 0 0;
 }
-
-/* .about-me-section {
-  display: flex;
-  margin-bottom: 10rem;
-  margin-top: 8rem;
-  margin-left: 5rem;
-}
-
-.presentation-container {
-  overflow: visible !important;
-  width: 40%;
-  padding: 5rem 0 6rem 6rem;
-}
-
-.presentation-container hgroup {
-  background-color: #f3b265;
-  border: 2px solid #fff;
-  width: 140%;
-  position: relative;
-  padding: 0.6rem 2rem;
-  margin-bottom: 2rem;
-  margin-top: -2rem;
-}
-
-.presentation-container h2 {
-  font-size: 2.4rem;
-  font-weight: 400;
-  color: #fff;
-  margin-bottom: 0.6rem;
-}
-
-.presentation-container h3 {
-  color: #fff;
-  font-size: 1.2rem;
-  font-weight: 300;
-  letter-spacing: 3px;
-}
-
-.presentation-container p {
-  width: 120%;
-  position: relative;
-  line-height: 1.7;
-}
-
-.img-container {
-  width: 28rem;
-  height: 28rem;
-  background-color: #eaeaea;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.img-container img {
-  width: 18rem;
-  border: 2px solid #fff;
-} */
 </style>
---
+++ src/components/AboutMeFact.vue
@@ -0,0 +1,146 @@
+<template>
+  <section class="about-me-fact">
+    <article class="fact-box">
+      <h3>Fakta</h3>
+      <ul>
+        <li>Ålder: 29 år</li>
+        <li>Bor: Göteborg</li>
+        <li>Egenskaper: noggrann, omtänksam &amp; driven</li>
+      </ul>
+    </article>
+    <section class="about-me-flex-container">
+      <article class="interests-box">
+        <h3>Och lite skryt-fakta</h3>
+        <ul>
+          <li>Cyklat från Umeå till Istanbul och sedan vidare Afrika</li>
+          <li>Vandrat hela Kungsleden + Kebne på 20 dagar</li>
+          <li>Flera JSM och USM medaljer i friidrott</li>
+          <li>240hp i Idrottsmedicin</li>
+        </ul>
+      </article>
+      <article class="funny-fact-box">
+        <h3>Intressen</h3>
+        <div class="interest-icons-container">
+          <figure>
+            <img src="../assets/icons/running.png" alt="running" />
+            <figcaption>Löpning</figcaption>
+          </figure>
+          <figure>
+            <img src="../assets/icons/hiking.png" alt="running" />
+            <figcaption>Äventyr</figcaption>
+          </figure>
+          <figure>
+            <img src="../assets/icons/climbing.png" alt="running" />
+            <figcaption>Klättring</figcaption>
+          </figure>
+          <figure>
+            <img src="../assets/icons/creating.png" alt="running" />
+            <figcaption>Skapande</figcaption>
+          </figure>
+          <figure>
+            <img src="../assets/icons/running.png" alt="running" />
+            <figcaption>Löpning</figcaption>
+          </figure>
+        </div>
+      </article>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AboutMeFact"
+};
+</script>
+
+<style scoped>
+.about-me-fact {
+  position: relative;
+  overflow: visible;
+}
+
+.about-me-flex-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.about-me-fact h3 {
+  font-size: 1.4rem;
+  font-weight: 300;
+  text-transform: uppercase;
+  color: #fff;
+  margin-bottom: 1rem;
+}
+
+.fact-box {
+  width: 30%;
+  height: 16rem;
+  background-color: rgba(88, 140, 126, 0.85);
+  background-color: rgba(196, 59, 59, 0.8);
+  border: 2px solid #fff;
+  padding: 2rem;
+
+  position: absolute;
+  top: -4rem;
+  z-index: 1;
+}
+
+.interests-box {
+  width: 60%;
+  height: 17rem;
+  /* background-color: #e3c9a4; */
+  background-color: #cecbcb;
+  border: 2px solid #fff;
+  padding: 2rem;
+  padding-left: 14rem;
+  overflow-y: hidden;
+}
+
+.interests-box li,
+.interests-box h3 {
+  color: #333333;
+}
+
+.interest-icons-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.interest-icons-container figure {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0.6rem;
+}
+
+.interest-icons-container figcaption {
+  color: #fff;
+  font-size: 12px;
+}
+
+.interest-icons-container img {
+  width: 2.5rem;
+}
+
+.funny-fact-box {
+  width: 30%;
+  height: 17rem;
+  background-color: #72a799;
+  border: 2px solid #fff;
+  padding: 2rem;
+  overflow-y: hidden;
+}
+
+ul {
+  padding: 0 1rem;
+}
+
+li {
+  font-size: 14px;
+  line-height: 1.4;
+  font-weight: 300;
+  color: #fff;
+  margin-bottom: 0.6rem;
+  overflow-x: visible;
+}
+</style>
